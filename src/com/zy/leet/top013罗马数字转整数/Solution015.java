4d469b991dc0dea9f5a7a6ae00094ffbdbe21ccc
ownzyuan <1076949424@qq.com>
2021-12-20 23:39:35 +0800

    罗马数字转整型

---
+++ src/com/zy/leet/top013罗马数字转整数/Solution015.java
@@ -0,0 +1,35 @@
+package com.zy.leet.top013罗马数字转整数;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution015 {
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        int i = romanToInt(s);
+        System.out.println(i);
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int nums = 0;
+        int i;
+        for(i = 0; i < s.length() - 1; i++)
+        {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1)))
+                nums -= map.get(s.charAt(i));
+            else
+                nums += map.get(s.charAt(i));
+        }
+        return nums + map.get(s.charAt(i));
+    }
+
+}
